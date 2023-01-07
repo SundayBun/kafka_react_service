eeed70dc1ebde30ef7a0cf3276f50d92fb56de2b
SundayBun <romashka5205@mail.ru>
2023-01-07 17:21:50 +0300

    update README.md

--- src/main/java/com/example/kafkaservice/repository/MyRepository.java
+++ src/main/java/com/example/kafkaservice/repository/MyRepository.java
@@ -11,7 +11,7 @@ import java.util.Date;
 @Repository
 public interface MyRepository extends ReactiveCrudRepository<MyEntity, Integer> {
 
-    @Query("SELECT org_id,lost, namespace FROM smev3new WHERE originMessageId=:originMessageId")
+    @Query("SELECT org_id,lost, namespace FROM table WHERE originMessageId=:originMessageId")
     Mono<MyEntity> findByMessageId(String originMessageId);
 
     @Query("UPDATE someTable SET date_from_queue=case WHEN date_from_queue IS NULL THEN now() ELSE date_from_queue END," +
